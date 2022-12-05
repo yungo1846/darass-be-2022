3a58ecf3d4890c94f2eb2cdf202ce3eb2af34e36
yungo1846 <yungo1468@naver.com>
2022-12-05 18:18:21 +0900

    feat: redis

--- src/index.ts
+++ src/index.ts
@@ -12,62 +12,78 @@ import { passportConfig } from './passports';
 import { commentRouter } from './routes/comments';
 import cors from 'cors';
 import { ProjectRouter } from './routes/project';
+import * as redis from 'redis';
+import _RedisStore from 'connect-redis';
 
 dotenv.config();
+
+const RedisStore = _RedisStore(session);
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+  legacyMode: true,
+});
 const app = express();
 
-app.set('port', process.env.PORT || 8000);
-app.use(express.json());
+const init = async () => {
+  app.set('port', process.env.PORT || 8000);
+  app.use(express.json());
 
-db.sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log('db 연결 성공');
-  })
-  .catch((error: unknown) => {
-    console.error(error);
+  db.sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log('db 연결 성공');
+    })
+    .catch((error: unknown) => {
+      console.error(error);
+    });
+
+  app.use(morgan('dev'));
+
+  await redisClient.connect();
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET ?? 'secret',
+      cookie: {
+        httpOnly: true,
+        secure: false,
+      },
+      store: new RedisStore({ client: redisClient }),
+    }),
+  );
+
+  passportConfig();
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'http://localhost:3000',
+    }),
+  );
+  app.use('/v1/users', userRouter);
+  app.use('/v1/auth', authRouter);
+  app.use('/v1/comments', commentRouter);
+  app.use('/v1/projects', ProjectRouter);
+
+  app.use('/', (req, res) => {
+    res.send('hello');
   });
 
-app.use(morgan('dev'));
+  app.use((req, res, next) => {
+    res.status(404);
+    res.send('404 Not Found');
+  });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET ?? 'secret',
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  }),
-);
+  app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+  });
+};
 
-passportConfig();
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:3000',
-  }),
-);
-app.use('/v1/users', userRouter);
-app.use('/v1/auth', authRouter);
-app.use('/v1/comments', commentRouter);
-app.use('/v1/projects', ProjectRouter);
-
-app.use('/', (req, res) => {
-  res.send('hello');
-});
-
-app.use((req, res, next) => {
-  res.status(404);
-  res.send('404 Not Found');
-});
-
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
-});
+init();
