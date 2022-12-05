9eabe9267e764ca7c67d65e9db0fd2fe23d57662
yungo1846 <yungo1468@naver.com>
2022-12-05 19:46:25 +0900

    fix: sequelize config import

--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,10 +1,5 @@
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../../config/config.json")[env];
-import { Sequelize } from "sequelize";
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/config.js')[env];
+import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+export const sequelize = new Sequelize(config.database, config.username, config.password, config);
